f89c7efcb9ce19bd10309d38456e53735cea23b1
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2022-04-22 15:01:34 +0200

    add task component and tasks list component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+};
+</script>
+<style scoped>
+.task {
+  background: #ffe6e6;
+  margin: 10px;
+  padding: 10px 20px;
+  cursor: pointer;
+  
+}
+.task.reminder {
+border-left: 5px solid green ;
+}
+</style>
