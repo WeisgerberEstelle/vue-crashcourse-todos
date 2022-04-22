57176f41ad6dc839cbab820ac2124df0abfd5716
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2022-04-22 14:24:11 +0200

    initialisation and add firsts components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header title="Task tracker"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+  },
+data() {
+  return{
+    tasks: []
   }
+},
+created() {
+  this.tasks=[
+    {
+      id:1,
+      text:'Doctors Appointment',
+      day:'March 1st',
+      reminder:true
+    },
+    {
+      id:2,
+      text:'Restaurant Grill 168',
+      day:'March 22th',
+      reminder:true
+    },
+    {
+      id:3,
+      text:'Coffee Shop',
+      day:'March 29th',
+      reminder:false
+    }
+  ]
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Bellota:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
+* {
+ box-sizing: border-box;
+ margin:0;
+ padding:0;
+}
+
+body {
+  font-family: 'Bellota';
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid skyblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: pink;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  box-shadow: 2px 2px 4px rgb(162, 188, 197);
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration:none;
+  font-size: 15px;
+  font-family: 'Bellota';
 }
 </style>
